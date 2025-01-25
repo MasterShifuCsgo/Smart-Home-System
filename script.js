97347b650e8f5e0df574f6662c3271ca98cbe07d
Kaspar Bergert <kasparbergert6@gmail.com>
2025-01-25 13:42:51 +0200

    Cycle through colors fixed, but with a weird twist to it.

--- script.js
+++ script.js
@@ -12,17 +12,34 @@ const CycleButton = document.querySelector(".Cycle");
 44 opacity
 */ 
 
+const decimaltoHexTable = {
+  0:"0",
+  1:"1",
+  2:"2",
+  3:"3",
+  4:"4",
+  5:"5",
+  6:"6",
+  7:"7",
+  8:"8",
+  9:"9",
+  10:"A",
+  11:"B",
+  12:"C",
+  13:"D",
+  14:"E",
+  15:"F"
+};
 
 function changeBoxColorTo(color){
-  BoxToColor.style.backgroundColor = color
-}
-  
-function decimalToHex(pattern){
+  r = color[0];
+  g = color[1];
+  b = color[2];
 
-  //convert into binary.
-  
-
+  BoxToColor.style.backgroundColor = `rgb(${r},${g},${b})`;
 }
+
+
 const listOfColors = {
   green: "#00FF00FF",
   blue: "#0000FFFF",
@@ -32,28 +49,54 @@ const listOfColors = {
 changeColorButton.addEventListener("click", function () {
 
 const color = colorInput.value;
-console.log(color);
 if(listOfColors[color] == null){
   return;
 }
+
 changeBoxColorTo(listOfColors[color]);
 });
 
-
-
-CycleButton.addEventListener("click", function (){
-
-  let color= {
-    1:255,
-    2:255,
-    3:255
-  };
-  while(true){
-
-    changeBoxColorTo(decimalToHex(color));
-    setTimeout(100);
+function MakeAllColorsSame(value, color){
+  for(let i = 0; i < color.length; i++){
+    color[i] = value;
   }
+}
+
+let direction = true;
+function addOneToColor(colorValue){
+  if(colorValue > 255){
+    direction = false;
+  }else if(colorValue < 0){
+    direction = true;
+  }
+  if(direction){
+    colorValue++;
+  }else{
+    colorValue--;
+  }
+  return colorValue
+}
+
+let color = [0,0,0];
+
+let num = 255*2;
+function CycleButtonFunction(){
+for(let i = 0; i < color.length; i++){
+  while(num > 0){
+    color[i] = addOneToColor(color[i]);
+    num--;
+  }
+  num = 255*2;
+}
+  //MakeAllColorsSame(colorValue, color)
+  changeBoxColorTo(color);
+};
+
+CycleButton.addEventListener("click", function() {
+  setInterval(CycleButtonFunction, 1);
 });
+
+
 
 
 
